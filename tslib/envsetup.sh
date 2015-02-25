7dda9f2fa03ddc2b1e365c821fde1f6a7d806afd
fogwizard <jwf1990@126.com>
2015-02-26 03:29:47 +0800

    Add tslib and add build script for tslib

---
+++ tslib/envsetup.sh
@@ -0,0 +1,3 @@
+current=$(dirname $('pwd'))
+export PATH="$PATH:$current/build/opt/EmbedSky/crosstools_3.4.5_softfloat/gcc-3.4.5-glibc-2.3.6/arm-linux/bin"
+#don't use hight cross compile!!
---
+++ tslib/startbuild.sh
@@ -0,0 +1,15 @@
+#1.install gun M4 under autogen dir (./configure make make install)
+#2.install autoconf in autoconf dir 
+#3.install automake in automake dir 
+#4.install libtool  in libtool  dir 
+# cancel the dir palse now ,so such install in system
+if [ "$1" = "?" ]; then
+	echo "Please Source Envsetup In The Father Dir And Then  Cast The $TOP Dir To This Script"
+	echo "(1)source build/envsetup.sh in this dir and don't source in father dir(too high arm-linux-g++)"
+	echo "(2)./startbuild ,As this script do it..." 
+	echo "(3)make && make install (install should be sudo )"
+	exit 1
+fi
+./autogen.sh
+echo "ac_cv_func_malloc_0_nonnull=yes" >arm-linux.cache
+./configure --host=arm-none-linux-gnueabi  --cache-file=arm-none-linux-gnueabi.cache --enable-inputapi=no  --prefix=$PWD/out
